6d136e4bb098a8240721b7f191c8624352cc953a
Olexandr Funtov <ffuntoff@gmail.com>
2013-04-03 18:24:46 +0300

    Task 1 implementation

---
+++ Merger/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Merger
+{
+    class Program
+    {
+
+       private static void ReadIntegers(string AFileName, List<int> AIntegersList)
+       {
+           int FileInteger;
+           using (StreamReader reader = File.OpenText(AFileName))
+           {
+               while (!reader.EndOfStream)
+               {
+                   if (!Int32.TryParse(reader.ReadLine(), out FileInteger))
+                       continue;
+                   AIntegersList.Add(FileInteger);
+               }
+           }
+       }
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Error: Please pass two filenames as params");
+                return;
+            }
+
+            string FilePath1 = args[0];
+            if (!File.Exists(FilePath1))
+            {
+                Console.WriteLine("Error: '" + FilePath1 + "' doesn't exist");
+                return;
+            }
+
+            string FilePath2 = args[1];
+            if (!File.Exists(FilePath2))
+            {
+                Console.WriteLine("Error: '" + FilePath2 + "' doesn't exist");
+                return;
+            }
+
+            List<int> IntegersList = new List<int>();
+            ReadIntegers(FilePath1, IntegersList);
+            ReadIntegers(FilePath2, IntegersList);
+            IntegersList.Sort();
+            File.WriteAllLines(args[2], IntegersList.Select(q => Convert.ToString(q)));
+        }
+    }
+}
